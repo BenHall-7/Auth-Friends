6c313e5124421dc22472f75adcf423b99d545aec
BenHall-7 <benjaminjahall@gmail.com>
2020-02-16 23:59:20 -0500

    Style the friend

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({name, age, email}) => {
-  return <div>
+  return <div className='friend'>
     <p>{name}</p>
     <p>Age: {age}</p>
     <p>{email}</p>
